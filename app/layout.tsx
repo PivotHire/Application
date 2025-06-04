4359dedd9e31e6730d5679ace4cde1864aa58ae0
CLCK0622 <zhongyi070622@163.com>
2025-06-04 13:37:33 +0800

    add: metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,52 @@ import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-    title: "PivotHire AI",
-    description: "AI Freelancing Platform",
+export const metadata = {
+    title: {
+        template: '%s | PivotHire AI',
+        default: 'PivotHire AI - Revolutionary Freelancing Platform',
+    },
+
+    description: 'The AI-driven freelancing platform for hiring match.',
+
+    keywords: ['startup', 'tech', 'hiring', 'labor market', 'HR', 'AI', 'freelancing', 'job matching'],
+
+    authors: [{name: 'Kevin Zhong'}, {name: 'Richard Liu'}, {name: 'Konrad Pan'}],
+
+    creator: 'PivotHire AI',
+
+    publisher: 'PivotHire AI',
+
+    openGraph: {
+        title: 'PivotHire AI - Revolutionary Freelancing Platform',
+        description: 'The AI-driven freelancing platform for hiring match.',
+        url: 'https://demo.pivothire.tech/',
+        siteName: 'PivotHire AI',
+        images: [
+            {
+                url: 'https://www.pivothire.tech/og-image-light(1200x630).png',
+                width: 1200,
+                height: 630,
+                alt: 'PivotHire AI',
+            },
+        ],
+        locale: 'en_US',
+        type: 'website',
+    },
+
+    robots: {
+        index: true,
+        follow: true,
+        nocache: false,
+    },
+
+    icons: {
+        icon: {
+            url: '/favicon.png',
+            type: 'image/png',
+            sizes: '96x96',
+        },
+    },
 };
 
 export default function RootLayout({
